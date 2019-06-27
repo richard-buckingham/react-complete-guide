5550adff46e85baa7cee238c3a6c92ab8ad3f30d
richie.buckingham <richie.buckingham@gmail.com>
2019-06-27 13:11:17 +1000

    add state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,39 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "rich",
+        age: 24
+      },
+      {
+        name: "rhonda",
+        age: 29
+      },
+      {
+        name: "elliott",
+        age: 20
+      }
+    ]
+  };
+
   render() {
+    // object destructuring
+    const { persons } = this.state;
+
     return (
       <div className="App">
         <h1>app component</h1>
-        <Person name="rich" age="45">
+        <button>Switch Name</button>
+        <Person name={persons[0].name} age={persons[0].age}>
+          Hobbies: Swimming
+        </Person>
+        <Person name={persons[1].name} age={persons[1].age}>
           Hobbies: Yoga
+        </Person>
+        <Person name={persons[2].name} age={persons[2].age}>
+          Hobbies: Running
         </Person>
       </div>
     );
