7d1fadaaa122ced1baa6fd04fd40b08e8033f503
richie.buckingham <richie.buckingham@gmail.com>
2019-06-27 12:31:40 +1000

    display children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import Person from "./Person/Person";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>app component</h1>
+        <Person name="rich" age="45">
+          Hobbies: Yoga
+        </Person>
       </div>
     );
   }
