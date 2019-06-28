2aedcb13bd6e6d690aab25c6deaa6b8ae00df10f
richie.buckingham <richie.buckingham@gmail.com>
2019-06-28 14:56:53 +1000

    update person

--- src/App.js
+++ src/App.js
@@ -17,23 +17,23 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = event => {
-    console.log("App component's nameChangedHandler method was called");
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    console.log("person to update = ", person);
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        {
-          name: "rich",
-          age: 24
-        },
-        {
-          name: event.target.value,
-          age: 29
-        },
-        {
-          name: "elliott",
-          age: 20
-        }
-      ]
+      persons: persons
     });
   };
 
@@ -76,6 +76,7 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
