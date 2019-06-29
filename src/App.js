1c7f6ca37c3f4bea0fdfb6e912093c7442307dec
richie.buckingham <richie.buckingham@gmail.com>
2019-06-29 10:47:14 +1000

    update background color dynamically

--- src/App.js
+++ src/App.js
@@ -57,7 +57,8 @@ class App extends Component {
 
   render() {
     const buttonStyle = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -82,6 +83,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      buttonStyle.backgroundColor = "red";
     }
 
     return (
