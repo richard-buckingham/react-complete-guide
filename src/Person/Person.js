e3a07a3bd10d354d8951bf7085173a65f0effb44
richie.buckingham <richie.buckingham@gmail.com>
2019-06-29 15:45:01 +1000

    update create-react-app to latest version and start using css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./Person.css";
+//import "./Person.css";
+import styles from "./Person.module.css"; // Import css modules stylesheet as styles
 
 const person = props => {
   return (
-    <div className="Person">
+    <div className={styles.Person}>
       <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} years old.
+        I'm {props.name} and I am {props.age} years old dude.
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
